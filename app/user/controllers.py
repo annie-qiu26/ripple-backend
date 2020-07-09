f082573be6ebfd60944527a89438bf9c4c198829
Brandon Wang <sourbrandon@gmail.com>
2020-07-09 16:57:17 -0700

    add function to get a user from cookies

--- app/user/controllers.py
+++ app/user/controllers.py
@@ -1,21 +1,12 @@
 from flask import Blueprint, request
+from app.user.models import User
 
 blueprint = Blueprint('user', __name__)
 
-# TODO: Delete example test function
-@blueprint.route('/api/user/test', methods=['GET'])
-def test():
-    body = request.json
-    return "<h1>Hello World!</h1>"
-
-@blueprint.route('/api/user', methods=['POST'])
-def create_user():
-    pass
-
-@blueprint.route('/api/user', methods=["GET"])
-def find_user(token):
-    pass
-
-@blueprint.route('/api/user', methods=["PUT"])
-def update_user():
-    pass
+def user_from_cookie(req):
+    userID = req.cookies.get("uid")
+    print(userID)
+    user = User.queryById(userID)
+    if user == None:
+        user = User()
+    return user
