9b64d9e137cf4f142f07ee2a65b690be4838b505
Brandon Wang <sourbrandon@gmail.com>
2020-07-09 16:56:54 -0700

    add functionality to user model

--- app/user/models.py
+++ app/user/models.py
@@ -1,21 +1,22 @@
 from app.database import Model
+from app import db
 
 class User(Model):
+    collection = db.users
+
     def __init__(self):
-        self.user_id = None # str
+        self._id = None # str
         self.email = None # str
-        self.location = None # Optional, str
-
-        self.created_at = None # DateTime
         
-        self.links = None # [link_id]
-        self.ripples = None # [ripple_id]
+        self.ripple_to_link = {} # ripple_id: link_id
+        self.link_child_index = {} # link_id: child index
 
-        self.total_unique_visitors = None # int
-        # ^ though how do we keep track of people visiting?
-        # (Google Analytics does this somehow?)
-        self.total_children = None # int 
-        self.total_descendants = None # int
-        self.total_depth = None # int
-        self.total_raised = None # int
-        self.total_miles = None # int
+    def get_link(self, ripple_id):
+        return self.ripple_to_link.get(ripple_id, None)
+
+    def set_ripple_link(self, ripple_id, link_id, index):
+        self.ripple_to_link[ripple_id] = link_id
+        self.link_child_index[link_id] = index
+    
+    def get_link_index(self, link_id):
+        return self.link_child_index.get(link_id, 0)
