c121bd2d4087a061f503ea2ccff82332a1106e8a
Brandon Wang <sourbrandon@gmail.com>
2020-07-10 21:09:01 -0700

    add search route to organizations

--- app/organization/models.py
+++ app/organization/models.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import re
 
 from app.database import Model
 from app import db
@@ -13,3 +14,19 @@ class Organization(Model):
         self.name = name # str, name of the organization
         self.url = url # str, url of the organization
         self.category = category # Optional
+
+    @classmethod
+    def fuzzySearch(cls, query):
+        try:
+            escaped = re.sub(r'[-[\]{}()*+?.,\\^$|#\s]', "\\$&", query)
+            regex = re.compile(escaped, flags=re.I)
+
+            documents = cls.collection.find({ "name": regex })
+            results = []
+            for i, doc in enumerate(documents):
+                doc['_id'] = str(doc['_id'])
+                results.append(doc)
+
+            return results
+        except Exception:
+            return None
